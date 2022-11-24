701f694e80e64ebbee149dc5f9d75cc4ceb5efc1
matalb23 <matalb23@gmail.com>
2022-11-24 13:15:01 -0300

    historico, filtros

--- src/app/historico/historico.page.ts
+++ src/app/historico/historico.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit,ViewChild  } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { SettingsService } from '../service/settings.service';
+import { FormGroup, FormBuilder } from "@angular/forms";
+
 @Component({
   selector: 'app-historico',
   templateUrl: './historico.page.html',
@@ -8,18 +10,44 @@ import { SettingsService } from '../service/settings.service';
 })
 export class HistoricoPage implements OnInit {
   servicios;
+  mainForm: FormGroup;
+  fechainicio;
+ // fechaInicioTemp;
+  fechafin;
+ // fechaABordoTemp;
   constructor(
+    public formBuilder: FormBuilder,
         private api: ApiService,
         private settings: SettingsService,
   ) { }
 
+  
   ngOnInit() {
+   this.inicializarForm();
   }
+  
+  inicializarForm() {
+    this.fechainicio=null;
+    this.fechafin=null;
+    this.mainForm = this.formBuilder.group({
+      'servicio': [''],
+      'fechainicio': [''],
+      'fechafin': [''],
+     'buque': [''],
+      'buqueBandera': ['']
+    })}
   ionViewDidEnter() {
         this.cargarDatos();
+        this.fechafin = null;
+        // this.fechaInicioTemp = null;
+        // this.fechaABordoTemp = null;
+        this.fechainicio = null;
+        
+
   }
   cargarDatos(){
-    this.api.get("api/historico?login="+this.settings.getValue(SettingsService.setting_User) ).subscribe((data) => {
+    const datos = { login:this.settings.getValue(SettingsService.setting_User) ,servicio:this.mainForm.value.servicio,fechafin:this.fechafin,fechainicio:this.fechainicio,buque:this.mainForm.value.buque,buqueBandera:this.mainForm.value.buqueBandera };
+    this.api.post("api/historico",datos ).subscribe((data) => {
       console.log("Busca historico :",data)    
       this.servicios=data;      
       }    
@@ -34,3 +62,7 @@ export class HistoricoPage implements OnInit {
   }
 
 }
+
+
+// if (this.FechaInicioNavegacionTemp != null)
+        // this.serviciodesdeApi.fechaInicioNavegacion = this.FechaInicioNavegacionTemp.replace("T", " ");
