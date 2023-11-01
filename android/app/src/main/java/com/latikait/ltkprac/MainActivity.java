0982c1defaf40790eeb35b81dacba4eafebe7dbd
matalb23 <matalb23@gmail.com>
2023-11-01 11:47:17 -0300

    Version 2072

--- android/app/src/main/java/com/latikait/ltkprac/MainActivity.java
+++ android/app/src/main/java/com/latikait/ltkprac/MainActivity.java
@@ -2,6 +2,4 @@ package com.latikait.ltkprac;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-      
-}
+public class MainActivity extends BridgeActivity {}
